9a23d4a73a3ddb2011e9dbc64f2cb7c09ab62067
Manjeet Singh <98460132+manjeetdeveloper@users.noreply.github.com>
2023-11-09 10:25:20 +0530

    new updated

---
+++ script.js
@@ -0,0 +1,15 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+registerBtn.addEventListener('click', () => {
+    container.classList.onclick("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
